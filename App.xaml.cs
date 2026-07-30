c96f3aa53af8050471f91448d6a84a58cf018def
Fadhil Fasalu Rahiman <ffasalurahiman@hof-university.de>
2026-07-30 06:45:35 +0200

    add: configure dependency injection container

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using WeatherApp.Services;
 using WeatherApp.ViewModels;
 
@@ -10,21 +11,29 @@ namespace WeatherApp
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider? _serviceProvider;
         private async void Application_Startup(object sender, StartupEventArgs e)
         {
             try
             {
+                var services = new ServiceCollection();
+
+                services.AddSingleton<IWeatherService, WeatherService>();
+                services.AddSingleton<IDatabaseService, DatabaseService>();
+                services.AddSingleton<IWeatherRecommendationService, WeatherRecommendationService>();
+
+                services.AddSingleton<MainViewModel>();
+                services.AddTransient<MainWindow>();
+
+                _serviceProvider = services.BuildServiceProvider();
+
                 await using var db = new AppDbContext();
                 await db.Database.MigrateAsync();
 
-                IWeatherService weatherService = new WeatherService();
-                IDatabaseService databaseService = new DatabaseService();
-                IWeatherRecommendationService recommendationService = new WeatherRecommendationService();
-
-                var mainViewModel = new MainViewModel(weatherService, databaseService, recommendationService);
+                var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
                 await mainViewModel.InitializeAsync();
 
-                var mainWindow = new MainWindow(mainViewModel);
+                var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
                 mainWindow.Show();
             }
             catch (Exception ex)
